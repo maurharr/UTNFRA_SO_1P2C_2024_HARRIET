28c0bec86b54b67b9bd23bff4645988e19ebb63d
Mauricio Harriet <mauricioharriet2@gmail.com>
2024-10-05 03:30:19 +0000

    fix: Más simplificación en el codigo

--- RTA_SCRIPT_Examen_20241004/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_A.sh
@@ -3,8 +3,8 @@
 # Creo una variable con el Path de la ubicación donde irán los archivos que cree
 DIR_ARCHIVOS=~/repogit/UTNFRA_SO_1P2C_2024_HARRIET/RTA_ARCHIVOS_Examen_20241004
 
-# Creo los directorios en una sola linea de codigo
-mkdir -p "$DIR_ARCHIVOS"/{alumno_1,alumno_2,alumno_3}/{parcial_1,parcial_2,parcial_3} "$DIR_ARCHIVOS"/profesores
+# Creo los directorios en una sola linea de codigo:
+mkdir -p "$DIR_ARCHIVOS"/{alumno_{1..3}/parcial_{1..3},/profesores}
 
 # Printeo un mensaje de finalización
 echo "Directorios creados con éxito en $DIR_ARCHIVOS"
