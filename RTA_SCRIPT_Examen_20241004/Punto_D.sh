ff41b14bd600ed1e5d2944d1c187de2735b5d153
Mauricio Harriet <mauricioharriet2@gmail.com>
2024-10-06 04:56:26 +0000

    feat: Resolución del Punto D

---
+++ RTA_SCRIPT_Examen_20241004/Punto_D.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR_ARCHIVOS=~/repogit/UTNFRA_SO_1P2C_2024_HARRIET/
+
+echo "Creando una estructura asimétrica..."
+mkdir -p "$DIR_ARCHIVOS"/Estructura_Asimetrica/{correo/cartas_{1..100},correo/carteros_{1..10},clientes/cartas_{1..100}}
+echo "Estructura asimétrica creada."
+
