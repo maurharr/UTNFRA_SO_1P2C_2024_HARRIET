deb1d160a7cee17e95494a22cac54e2ed09dfa9b
Mauricio Harriet <mauricioharriet2@gmail.com>
2024-10-06 20:32:01 +0000

    fix: Correxión en el path de los archivos

--- RTA_SCRIPT_Examen_20241004/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_C.sh
@@ -17,17 +17,17 @@ echo "Usuarios creados."
 
 
 echo "Asignandole a los directorios sus usuarios y grupos."
-chown p1c2_2024_A1.p1c2_2024_gAlumno /Examenes-UTN/alumno_1/
-chown p1c2_2024_A2.p1c2_2024_gAlumno /Examenes-UTN/alumno_2/
-chown p1c2_2024_A3.p1c2_2024_gAlumno /Examenes-UTN/alumno_3/
-chown p1c2_2024_P1.p1c2_2024_gProfesores /Examenes-UTN/profesores/
+sudo chown p1c2_2024_A1.p1c2_2024_gAlumno $DIR_ARCHIVOS/alumno_1/
+sudo chown p1c2_2024_A2.p1c2_2024_gAlumno $DIR_ARCHIVOS/alumno_2/
+sudo chown p1c2_2024_A3.p1c2_2024_gAlumno $DIR_ARCHIVOS/alumno_3/
+sudo chown p1c2_2024_P1.p1c2_2024_gProfesores $DIR_ARCHIVOS/profesores/
 echo "Usuarios y grupos asignados."
 
 
 echo "Asigno los permisos a los directorios."
-chmod 750 $DIR_ARCHIVOS/alumno_1/
-chmod 760 $DIR_ARCHIVOS/alumno_2/
-chmod 700 $DIR_ARCHIVOS/alumno_3/
-chmod 775 $DIR_ARCHIVOS/profesores/
+sudo chmod 750 $DIR_ARCHIVOS/alumno_1/
+sudo chmod 760 $DIR_ARCHIVOS/alumno_2/
+sudo chmod 700 $DIR_ARCHIVOS/alumno_3/
+sudo chmod 775 $DIR_ARCHIVOS/profesores/
 echo "Permisos asignados."
 
