9b417ea96c142a89d38388cb0fdafe472b106c8d
Mauricio Harriet <mauricioharriet2@gmail.com>
2024-10-08 02:46:59 +0000

    fix: Correxión del path para la creación de los directorios.

--- RTA_SCRIPT_Examen_20241004/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_A.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Creo una variable con el Path de la ubicación donde irán los archivos que cree
-DIR_ARCHIVOS=~/
-
 # Creo los directorios en una sola linea de codigo:
-mkdir -p "$DIR_ARCHIVOS"/Examenes-UTN/{alumno_{1..3}/parcial_{1..3},/profesores}
+sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},/profesores}
 
 # Printeo un mensaje de finalización
-echo "Directorios creados con éxito en $DIR_ARCHIVOS"
+echo "Directorios creados con éxito."
